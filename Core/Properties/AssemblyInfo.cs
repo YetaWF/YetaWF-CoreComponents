f1be6d1f6c6caf50fad05c84966ea53dd6abcfd6
YetaWF <mikevdm@YetaWF.com>
2016-08-21 20:46:34 -0400

    prepare for 1.0.5

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.4.0")]
+[assembly: AssemblyVersion("1.0.5.0")]
 
 [assembly: Package(PackageTypeEnum.Core, "YetaWF")]
 [assembly: PackageInfo("http://YetaWF.com/UpdateServer",
