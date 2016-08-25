051b9ef16dca66ed9a448a28428e12e523004f05
YetaWF <mikevdm@YetaWF.com>
2016-08-25 07:44:33 -0400

    area & packagedomain validations

---
+++ Core/Models/Attributes/AreaValidationAttribute.cs
@@ -0,0 +1,24 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using YetaWF.Core.Localize;
+
+namespace YetaWF.Core.Models.Attributes {
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class AreaValidationAttribute : System.ComponentModel.DataAnnotations.RegularExpressionAttribute, IClientValidatable {
+
+        private static string __ResStr(string name, string defaultValue, params object[] parms) { return ResourceAccess.GetResourceString(typeof(Resources), name, defaultValue, parms); }
+
+        public AreaValidationAttribute() : base(@"^\s*[a-z0-9]{1,50}_[a-z0-9]{1,50}\s*$") {
+            ErrorMessage = __ResStr("valArea", "The area specified is invalid - Please use only letters and numbers in the format domain_product");
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context) {
+            ErrorMessage = __ResStr("valArea2", "The area specified is invalid ({0}) - Please use only letters and numbers in the format domain_product", AttributeHelper.GetPropertyCaption(metadata));
+            return new[] { new ModelClientValidationRegexRule(ErrorMessage, this.Pattern) };
+        }
+    }
+}
---
+++ Core/Models/Attributes/PackageDomainValidationAttribute.cs
@@ -0,0 +1,24 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using YetaWF.Core.Localize;
+
+namespace YetaWF.Core.Models.Attributes {
+
+    [AttributeUsage(AttributeTargets.Property, AllowMultiple = false, Inherited = true)]
+    public class PackageDomainValidationAttribute : System.ComponentModel.DataAnnotations.RegularExpressionAttribute, IClientValidatable {
+
+        private static string __ResStr(string name, string defaultValue, params object[] parms) { return ResourceAccess.GetResourceString(typeof(Resources), name, defaultValue, parms); }
+
+        public PackageDomainValidationAttribute() : base(@"^\s*[a-z0-9]{1,50}\s*$") {
+            ErrorMessage = __ResStr("valPkgDomain", "The domain specified is invalid - Please use only letters and numbers");
+        }
+
+        public IEnumerable<ModelClientValidationRule> GetClientValidationRules(ModelMetadata metadata, ControllerContext context) {
+            ErrorMessage = __ResStr("valPkgDomain2", "The domain specified is invalid ({0}) - Please use only letters and numbers", AttributeHelper.GetPropertyCaption(metadata));
+            return new[] { new ModelClientValidationRegexRule(ErrorMessage, this.Pattern) };
+        }
+    }
+}
