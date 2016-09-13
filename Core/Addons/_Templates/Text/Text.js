c3e39821fa2ada3232f3b06048bf4b3f840f010b
YetaWF <mikevdm@YetaWF.com>
2016-09-13 18:46:30 -0400

    localize fix dup Y_Confirms

--- Core/Addons/_Templates/Text/Text.js
+++ Core/Addons/_Templates/Text/Text.js
@@ -5,10 +5,6 @@ var _YetaWF_Text_ClipBoard = undefined;
 
 YetaWF_Text_Clip.init = function() {
     'use strict';
-    if (_YetaWF_Text_ClipBoard !== undefined) {
-        _YetaWF_Text_ClipBoard.destroy();
-        _YetaWF_Text_ClipBoard = undefined;
-    }
     if ($('.yt_text_copy').length > 0) {
         var _YetaWF_Text_ClipBoard = new Clipboard('.yt_text_copy', {
             target: function (trigger) {
@@ -16,18 +12,12 @@ YetaWF_Text_Clip.init = function() {
             },
         });
         _YetaWF_Text_ClipBoard.on('success', function (e) {
-            Y_Confirm("Copied to clipboard");//$$localize
+            Y_Confirm(YLocs.Text.CopyToClip);
         });
     }
 };
 
 $(document).ready(function () {
-    'use strict';
     YetaWF_Text_Clip.init();
-    if (typeof YetaWF_Forms !== 'undefined' && YetaWF_Forms != undefined) {
-        YetaWF_Forms.partialFormActionsAll.push({
-            callback: YetaWF_Text_Clip.init
-        });
-    }
 });
 
