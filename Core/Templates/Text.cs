6d2e3e3c4010fdff0ca01a29d9b72d14e63b57e9
YetaWF <mikevdm@YetaWF.com>
2016-09-13 19:18:26 -0400

    localization for text.js

---
+++ Core/Templates/Text.cs
@@ -0,0 +1,19 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+using YetaWF.Core.Localize;
+using YetaWF.Core.Pages;
+using YetaWF.Core.Support;
+
+namespace YetaWF.Core.Addons.Templates {
+
+    public class Text : IAddOnSupport {
+
+        public void AddSupport(YetaWFManager manager) {
+
+            ScriptManager scripts = manager.ScriptManager;
+
+            scripts.AddLocalization("Text", "CopyToClip", this.__ResStr("copyToClip", "Copied to clipboard"));
+
+        }
+    }
+}
