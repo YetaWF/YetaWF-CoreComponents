c3eb99062f626ccfddba5318557080aaa0e05026
YetaWF <mikevdm@YetaWF.com>
2016-09-13 17:44:38 -0400

    clipboard support for Text templates Clipboard.js addon

---
+++ Core/Addons/_Templates/Text/Text.js
@@ -0,0 +1,33 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+var YetaWF_Text_Clip = {};
+var _YetaWF_Text_ClipBoard = undefined;
+
+YetaWF_Text_Clip.init = function() {
+    'use strict';
+    if (_YetaWF_Text_ClipBoard !== undefined) {
+        _YetaWF_Text_ClipBoard.destroy();
+        _YetaWF_Text_ClipBoard = undefined;
+    }
+    if ($('.yt_text_copy').length > 0) {
+        var _YetaWF_Text_ClipBoard = new Clipboard('.yt_text_copy', {
+            target: function (trigger) {
+                return trigger.previousElementSibling;
+            },
+        });
+        _YetaWF_Text_ClipBoard.on('success', function (e) {
+            Y_Confirm("Copied to clipboard");//$$localize
+        });
+    }
+};
+
+$(document).ready(function () {
+    'use strict';
+    YetaWF_Text_Clip.init();
+    if (typeof YetaWF_Forms !== 'undefined' && YetaWF_Forms != undefined) {
+        YetaWF_Forms.partialFormActionsAll.push({
+            callback: YetaWF_Text_Clip.init
+        });
+    }
+});
+
--- Core/Views/Shared/Support/TextBoxHelper.cs
+++ Core/Views/Shared/Support/TextBoxHelper.cs
@@ -1,9 +1,13 @@
 ﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
 
 using System.Web.Mvc;
+using YetaWF.Core.Addons;
+using YetaWF.Core.Localize;
 using YetaWF.Core.Models;
 using YetaWF.Core.Models.Attributes;
 using YetaWF.Core.Pages;
+using YetaWF.Core.Skins;
+using YetaWF.Core.Support;
 
 namespace YetaWF.Core.Views.Shared {
 
@@ -11,7 +15,15 @@ namespace YetaWF.Core.Views.Shared {
 
     public static class TextBoxHelper {
 
+        private static YetaWFManager Manager { get { return YetaWFManager.Manager; } }
+
+        private static string __ResStr(string name, string defaultValue, params object[] parms) { return ResourceAccess.GetResourceString(typeof(TextBoxHelper), name, defaultValue, parms); }
+
         public static MvcHtmlString RenderTextBox(this HtmlHelper htmlHelper, string name, string text, int dummy = 0, object HtmlAttributes = null, string ModelNameOverride = null, bool Validation = true) {
+
+            HtmlBuilder hb = new HtmlBuilder();
+
+            bool copy = htmlHelper.GetControlInfo<bool>("", "Copy", false);
 
             TagBuilder tag = new TagBuilder("input");
             htmlHelper.FieldSetup(tag, name, HtmlAttributes: HtmlAttributes, ModelNameOverride: ModelNameOverride, Validation: Validation);
@@ -28,18 +40,47 @@ namespace YetaWF.Core.Views.Shared {
             tag.MergeAttribute("type", "text");
             tag.MergeAttribute("value", text);
 
-            return MvcHtmlString.Create(tag.ToString(TagRenderMode.StartTag));
+            hb.Append(tag.ToString(TagRenderMode.StartTag));
+
+            if (copy) {
+                Manager.AddOnManager.AddAddOnGlobal("clipboardjs.com", "clipboard");// add clipboard support
+                SkinImages skinImages = new SkinImages();
+                string imageUrl = skinImages.FindIcon_Template("Copy.png", YetaWF.Core.Controllers.AreaRegistration.CurrentPackage, "Text");
+                TagBuilder tagImg = ImageHelper.BuildKnownImageTag(imageUrl, title: __ResStr("ttCopy", "Copy to Clipboard"), alt: __ResStr("altCopy", "Copy to Clipboard"));
+                tagImg.AddCssClass("yt_text_copy");
+                hb.Append(tagImg.ToString(TagRenderMode.StartTag));
+            }
+            return MvcHtmlString.Create(hb.ToString());
         }
         public static MvcHtmlString RenderTextBoxDisplay(this HtmlHelper htmlHelper, string name, string text, int dummy = 0, object HtmlAttributes = null, string ModelNameOverride = null) {
+
+            HtmlBuilder hb = new HtmlBuilder();
+
+            bool copy = htmlHelper.GetControlInfo<bool>("", "Copy", true);
+            bool rdonly = htmlHelper.GetControlInfo<bool>("", "ReadOnly", false);
 
             TagBuilder tag = new TagBuilder("input");
             htmlHelper.FieldSetup(tag, name, HtmlAttributes: HtmlAttributes, ModelNameOverride: ModelNameOverride, Validation: false, Anonymous: true);
 
             tag.MergeAttribute("type", "text");
             tag.MergeAttribute("value", text);
+            if (copy || rdonly)
+                tag.MergeAttribute("readonly", "readonly");
+            else
+                tag.MergeAttribute("disabled", "disabled");
 
-            return MvcHtmlString.Create(tag.ToString(TagRenderMode.StartTag));
+            hb.Append(tag.ToString(TagRenderMode.StartTag));
+
+            if (copy) {
+                Manager.AddOnManager.AddAddOnGlobal("clipboardjs.com", "clipboard");// add clipboard support
+                SkinImages skinImages = new SkinImages();
+                string imageUrl = skinImages.FindIcon_Template("Copy.png", YetaWF.Core.Controllers.AreaRegistration.CurrentPackage, "Text");
+                TagBuilder tagImg = ImageHelper.BuildKnownImageTag(imageUrl, title: __ResStr("ttCopy", "Copy to Clipboard"), alt: __ResStr("altCopy", "Copy to Clipboard"));
+                tagImg.AddCssClass("yt_text_copy");
+                hb.Append(tagImg.ToString(TagRenderMode.StartTag));
+            }
+            return MvcHtmlString.Create(hb.ToString());
         }
-
     }
 }
+;
