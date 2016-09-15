b47b60c9acfe480f607db437d1fe92116c36b6ab
YetaWF <mikevdm@YetaWF.com>
2016-09-14 23:16:37 -0400

    cleanup

--- Core/Addons/_Templates/Text/Text.js
+++ Core/Addons/_Templates/Text/Text.js
@@ -1,23 +1,19 @@
 ﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
 
-var YetaWF_Text_Clip = {};
-var _YetaWF_Text_ClipBoard = undefined;
-
-YetaWF_Text_Clip.init = function() {
-    'use strict';
-    if ($('.yt_text_copy').length > 0) {
-        var _YetaWF_Text_ClipBoard = new Clipboard('.yt_text_copy', {
-            target: function (trigger) {
-                return trigger.previousElementSibling;
-            },
-        });
-        _YetaWF_Text_ClipBoard.on('success', function (e) {
-            Y_Confirm(YLocs.Text.CopyToClip);
-        });
-    }
-};
-
 $(document).ready(function () {
-    YetaWF_Text_Clip.init();
+    'use strict';
+    function initClip() {
+        if ($('.yt_text_copy').length > 0) {
+            var clipBoard = new Clipboard('.yt_text_copy', {
+                target: function (trigger) {
+                    return trigger.previousElementSibling;
+                },
+            });
+            clipBoard.on('success', function (e) {
+                Y_Confirm(YLocs.Text.CopyToClip);
+            });
+        }
+    };
+    initClip();
 });
 
