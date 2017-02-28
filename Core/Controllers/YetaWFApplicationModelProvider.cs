50e15113f6b54f21998668a0ffcbbab4c8cb8354
YetaWF <mikevdm@YetaWF.com>
2017-02-28 09:22:46 -0500

    missed files

---
+++ Core/Controllers/YetaWFApplicationModelProvider.cs
@@ -0,0 +1,38 @@
+﻿/* Copyright © 2017 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+#if MVC6
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApplicationModels;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.Extensions.Options;
+using System.Reflection;
+using YetaWF.Core.Packages;
+using YetaWF.Core.Support;
+
+namespace YetaWF.Core.Controllers {
+
+    public class YetaWFApplicationModelProvider : DefaultApplicationModelProvider {
+        public YetaWFApplicationModelProvider(IOptions<MvcOptions> mvcOptionsAccessor) : base(mvcOptionsAccessor) { }
+
+        // This is called at startup for all controllers - so no need to cache the data we collect
+        protected override ControllerModel CreateControllerModel(TypeInfo typeInfo) {
+            ControllerModel ctrlModel = base.CreateControllerModel(typeInfo);
+            ctrlModel.RouteValues.Add("area", Lookup(typeInfo)); // add our area name based on the package containing the controller
+            return ctrlModel;
+        }
+#if NOTUSED // provided for future expansion
+        protected override ActionModel CreateActionModel(TypeInfo typeInfo, MethodInfo methodInfo) {
+            ActionModel actionModel = base.CreateActionModel(typeInfo, methodInfo);
+            return actionModel;
+        }
+#endif
+        private string Lookup(TypeInfo typeInfo) {
+            Package package = Package.GetPackageFromType(typeInfo.AsType());
+            if (package == null)
+                throw new InternalError("No area found for controller type {0}", typeInfo.FullName);
+            return package.AreaName;
+        }
+    }
+}
+#endif
---
+++ Core/Models/Attributes/AreaAttribute.cs
@@ -0,0 +1,18 @@
+﻿/* Copyright © 2017 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+#if MVC6
+
+using System;
+
+namespace YetaWF.Core.Models.Attributes {
+
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    [Obsolete("Do not use Area() - This is automatically defined by YetaWF")]
+    public class AreaAttribute : System.Attribute {
+        public AreaAttribute(string areaName) { }
+    }
+
+}
+
+#else
+#endif
