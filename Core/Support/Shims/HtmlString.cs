92e317d5f86e0007481d8250f5f3535f075eb6d8
YetaWF <mikevdm@YetaWF.com>
2017-02-28 18:23:59 -0500

    missing file

---
+++ Core/Support/Shims/HtmlString.cs
@@ -0,0 +1,12 @@
+﻿/* Copyright © 2017 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Licensing */
+
+namespace YetaWF.Core.Support {
+
+#if MVC6
+#else
+    public static class HtmlStringExtender  {
+
+
+    }
+#endif
+}
