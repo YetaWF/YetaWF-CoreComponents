3c14edc86682cd1c19c0005f6f6efa2a9504f9bf
YetaWF <mikevdm@YetaWF.com>
2017-04-16 12:02:31 -0400

    nofollow

--- Core/ViewsCode/Support/PageSelectionHelper.cs
+++ Core/ViewsCode/Support/PageSelectionHelper.cs
@@ -56,6 +56,7 @@ namespace YetaWF.Core.Views.Shared {
 
             tag.MergeAttribute("href", (page != null ? page.EvaluatedCanonicalUrl : ""));
             tag.MergeAttribute("target", "_blank");
+            tag.MergeAttribute("rel", "nofollow");
             tag.Attributes.Add(Basics.CssTooltip, __ResStr("linkTT", "Click to preview the page in a new window - not all pages can be displayed correctly and may require additional parameters"));
 
             // image
