9ce8c6329347d3ca3815c014b34d9868a176fad1
YetaWF <mikevdm@YetaWF.com>
2017-04-23 08:01:58 -0400

    using

--- Core/Models/Attributes/RegexValidationBaseAttribute.cs
+++ Core/Models/Attributes/RegexValidationBaseAttribute.cs
@@ -1,13 +1,13 @@
 ﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
 using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using YetaWF.Core.Localize;
 using YetaWF.Core.Support;
 #if MVC6
 using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 #else
-using System.Collections.Generic;
 using System.Web.Mvc;
 #endif
 
