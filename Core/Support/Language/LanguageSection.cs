ca958f5ea57c663a8a74e46adcd7be958514926e
YetaWF <mikevdm@YetaWF.com>
2017-05-21 11:37:31 -0400

    move language init back into languages package (low level service provider)

--- Core/Support/Language/LanguageSection.cs
+++ Core/Support/Language/LanguageSection.cs
@@ -1,6 +1,5 @@
 ﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
-using System;
 using System.Collections.Generic;
 using System.IO;
 using YetaWF.Core.Support;
@@ -14,29 +13,13 @@ namespace YetaWF.Core.Language {
         public string Description { get; set; }
     }
 
-    public class LanguageSection : IInitializeApplicationStartup {
-
-        // IInitializeApplicationStartup
-        // IInitializeApplicationStartup
-        // IInitializeApplicationStartup
-
-        public void InitializeApplicationStartup() {
-            string rootFolder;
-#if MVC6
-            rootFolder = YetaWFManager.RootFolderWebProject;
-#else
-            rootFolder = YetaWFManager.RootFolder;
-#endif
-            Init(Path.Combine(rootFolder, Globals.DataFolder, LanguageSettingsFile));
-        }
+    public static class LanguageSection {
 
         // Languages
 
-        public const string LanguageSettingsFile = "LanguageSettings.json";
-
         public static LanguageEntryElementCollection Languages { get; set; }
 
-        private void Init(string settingsFile) {
+        public static void Init(string settingsFile) {
             if (!File.Exists(settingsFile))
                 throw new InternalError("Language settings not defined - file {0} not found", settingsFile);
             SettingsFile = settingsFile;
@@ -47,7 +30,7 @@ namespace YetaWF.Core.Language {
         private static string SettingsFile;
         private static dynamic Settings;
 
-        private LanguageEntryElementCollection GetLanguages() {
+        private static LanguageEntryElementCollection GetLanguages() {
             dynamic LanguageSection = Settings["LanguageSection"];
             LanguageEntryElementCollection list = new LanguageEntryElementCollection();
             foreach (var t in LanguageSection["Languages"]) {
