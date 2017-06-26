30348f91fa7d9c1de7c9bdeceea145f978af4e1e
YetaWF <mikevdm@YetaWF.com>
2017-06-26 11:28:02 -0400

    CardId->CVV

--- Core/Models/Attributes/CreditCardCCVValidationAttribute.cs
+++ Core/Models/Attributes/CreditCardCCVValidationAttribute.cs
@@ -12,8 +12,8 @@ namespace YetaWF.Core.Models.Attributes {
         private static string __ResStr(string name, string defaultValue, params object[] parms) { return ResourceAccess.GetResourceString(typeof(Resources), name, defaultValue, parms); }
 
         public CreditCardCCVValidationAttribute() : base(@"^\s*([0-9]{3,4})\s*$",
-                __ResStr("valCCV", "The card ID is invalid - The Card ID is printed on the back of your credit card - Usually the last 4 digits of your card number, followed by the card ID (3-4 digits) are shown. If your credit card has such a security code, please enter it here (if you do not enter it, your order may be delayed or rejected) - If your card has no such code, simply leave this field empty"),
-                __ResStr("valCCV2", "The card ID is invalid (field '{0}') - The Card ID is printed on the back of your credit card - Usually the last 4 digits of your card number, followed by the card ID (3-4 digits) are shown. If your credit card has such a security code, please enter it here (if you do not enter it, your order may be delayed or rejected) - If your card has no such code, simply leave this field empty")
+                __ResStr("valCCV", "The CVV (Card Verification Value) is invalid - The CVV is printed on the back of your credit card - Usually the last 4 digits of your card number, followed by the CVV (3-4 digits) are shown. If your credit card has such a security code, please enter it here (if you do not enter it, your order may be delayed or rejected) - If your card has no such code, simply leave this field empty"),
+                __ResStr("valCCV2", "The CCV (Card Verification Value) is invalid (field '{0}') - The CVV is printed on the back of your credit card - Usually the last 4 digits of your card number, followed by the CVV (3-4 digits) are shown. If your credit card has such a security code, please enter it here (if you do not enter it, your order may be delayed or rejected) - If your card has no such code, simply leave this field empty")
             ) { }
     }
 }
