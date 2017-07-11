1f0c8a695e4f56f2106c295fc8347bac3b1770c2
YetaWF <mikevdm@YetaWF.com>
2017-07-11 12:06:58 -0400

    defer just not worth it with all the timing issues everywhere for such a small item, not to mention UPS

--- Core/Addons/_Templates/RecaptchaV2/recaptchav2.js
+++ Core/Addons/_Templates/RecaptchaV2/recaptchav2.js
@@ -2,7 +2,7 @@
 
 var YetaWF_Core_RecaptchaV2 = {};
 
-var YetaWF_Core_RecaptchaV2_onLoad = function ($tag) {
+YetaWF_Core_RecaptchaV2.onLoad = function ($tag) {
     'use strict';
     $('.yt_recaptchav2', $tag).each(function () {
         grecaptcha.render(this, {
@@ -13,8 +13,9 @@ var YetaWF_Core_RecaptchaV2_onLoad = function ($tag) {
     });
 };
 
- YetaWF_Basics.whenReady.push({
-     callback: function ($tag) {
-         YetaWF_Core_RecaptchaV2_onLoad($tag);
-     }
- });
+YetaWF_Basics.whenReady.push({
+    callback: function ($tag) {
+        //alert('hi');
+        YetaWF_Core_RecaptchaV2.onLoad($tag);
+    }
+});
