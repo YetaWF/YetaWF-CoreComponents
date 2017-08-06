1945040bc53f86a660f8a88a93472b7b70e1e1e8
YetaWF <mikevdm@YetaWF.com>
2017-08-06 14:41:12 -0400

    prepare for 2.6.0

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -16,7 +16,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("2.5.2.0")]
+[assembly: AssemblyVersion("2.6.0.0")]
 
 [assembly: Package(PackageTypeEnum.Core, "YetaWF")]
 
