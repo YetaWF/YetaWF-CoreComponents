37072655ce8359832cfa618069a75364d293b362
YetaWF <mikevdm@YetaWF.com>
2018-01-14 09:18:59 -0500

    abandoned signalr on asp.net core for now, reveisit with 2.1.0 preview

--- Core/Startup/StartupSignalr.cs
+++ Core/Startup/StartupSignalr.cs
@@ -1,18 +1,26 @@
 ﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
+using YetaWF.Core.Controllers;
+using YetaWF.Core.Site;
+using Microsoft.Extensions.DependencyInjection;
 #if MVC6
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.Http;
 #else
-
 using Microsoft.AspNet.SignalR;
 using Owin;
 using System.Web;
-using YetaWF.Core.Controllers;
-using YetaWF.Core.Site;
+#endif
 
 namespace YetaWF.Core.Support {
 
-    public class Signalr : IInitializeOwinStartup {
-
+    public class Signalr
+#if MVC6
+#else
+        : IInitializeOwinStartup 
+#endif
+    {
         public static readonly string SignalRUrl = "/__signalr";
 
         public static void Use() {
@@ -20,6 +28,22 @@ namespace YetaWF.Core.Support {
             YetaWFManager.Manager.ScriptManager.AddConfigOption("Basics", "SignalRUrl", SignalRUrl);
         }
 
+#if MVC6
+        public static void ConfigureServices(IServiceCollection services) {
+            services.AddSignalR();
+        }
+
+        public static void ConfigureHubs(IApplicationBuilder app) {
+            // Find all hubs
+            //**TODO:
+            // This is where I stopped as there are quite a few missing pieces (1/14/2018, like Others, Caller, etc.) - As I don't
+            // currently need signalr support on asp.net core, we'll just wait. https://github.com/aspnet/SignalR/issues/394
+
+            app.UseSignalR((routes) => {
+               //TODO: routes.MapHub<xxx>(SignalRUrl + "/" + "xxxx");
+            });
+        }
+#else
         public void InitializeOwinStartup(IAppBuilder app) {
             HubConfiguration hubConfig = new HubConfiguration();
 #if DEBUG
@@ -28,15 +52,25 @@ namespace YetaWF.Core.Support {
             hubConfig.EnableJavaScriptProxies = false;
             app.MapSignalR(SignalRUrl, hubConfig);
         }
+#endif
 
         /// <summary>
-        /// Set up environment info for signalr requests.
+        /// Set up environment info for SignalR requests.
         /// </summary>
         public static YetaWFManager SetupEnvironment() {
             if (YetaWFManager.HaveManager) return YetaWFManager.Manager;
+            YetaWFManager manager;
+            string host;
+#if MVC6
+            HttpContext context = YetaWFManager.HttpContextAccessor.HttpContext;
+            HttpRequest httpReq = context.Request;
+            host = httpReq.Host.Host;
+            manager = YetaWFManager.MakeInstance(context, host);
+#else
             HttpRequest httpReq = HttpContext.Current.Request;
-            string host = httpReq.Url.Host;
-            YetaWFManager manager = YetaWFManager.MakeInstance(host);
+            host = httpReq.Url.Host;
+            manager = YetaWFManager.MakeInstance(host);
+#endif
             manager.CurrentSite = SiteDefinition.LoadSiteDefinition(host);
             if (manager.CurrentSite == null) throw new InternalError("No site definition for {0}", host);
             YetaWFController.SetupEnvironmentInfo();
@@ -44,5 +78,3 @@ namespace YetaWF.Core.Support {
         }
     }
 }
-
-#endif
