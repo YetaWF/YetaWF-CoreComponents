7a335e1e15984aad825bebec26b58d9046fd6829
YetaWF <mikevdm@YetaWF.com>
2018-04-20 18:14:28 -0400

    seriously, WTF

--- Core/Support/Security/RSACrypto.cs
+++ Core/Support/Security/RSACrypto.cs
@@ -4,6 +4,9 @@ using System;
 using System.Security.Cryptography;
 using System.Text;
 using YetaWF.Core.Support;
+#if NETCOREAPP
+using System.Xml;
+#endif
 
 namespace YetaWF.Core.Security {
     public static class RSACrypto {
@@ -14,8 +17,13 @@ namespace YetaWF.Core.Security {
         public static void MakeNewKeys(out string publicKey, out string privateKey) {
             using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider()) {
                 rsa.PersistKeyInCsp = false;
+#if NETCOREAPP
+                privateKey = rsa.TranslateToXmlString(true);
+                publicKey = rsa.TranslateToXmlString(false);
+#else
                 privateKey = rsa.ToXmlString(true);
                 publicKey = rsa.ToXmlString(false);
+#endif
             }
 #if DEBUG
             string Token = "User1Forever";
@@ -24,7 +32,7 @@ namespace YetaWF.Core.Security {
             string decryptedText;
             RSACrypto.Decrypt(privateKey, encryptedText, out decryptedText);
             if (decryptedText != Token)
-                throw new InternalError("Encryption/decription test failure");
+                throw new InternalError("Encryption/decryption test failure");
 #endif
         }
 
@@ -34,7 +42,11 @@ namespace YetaWF.Core.Security {
             try {
                 using (RSACryptoServiceProvider rsaProvider = new RSACryptoServiceProvider()) {
                     rsaProvider.PersistKeyInCsp = false;
+#if NETCOREAPP
+                    rsaProvider.TranslateFromXmlString(publicKeyXml);
+#else
                     rsaProvider.FromXmlString(publicKeyXml);
+#endif
                     byte[] plainBytes = Encoding.Unicode.GetBytes(plainText);
                     byte[] encryptedBytes = rsaProvider.Encrypt(plainBytes, false);
 
@@ -60,7 +72,11 @@ namespace YetaWF.Core.Security {
 
                 using (RSACryptoServiceProvider rsaProvider = new RSACryptoServiceProvider()) {
                     rsaProvider.PersistKeyInCsp = false;
+#if NETCOREAPP
+                    rsaProvider.TranslateFromXmlString(privateKeyXml);
+#else
                     rsaProvider.FromXmlString(privateKeyXml);
+#endif
                     byte[] plainBytes  = rsaProvider.Decrypt(encryptedBytes, false);
                     plainText = Encoding.Unicode.GetString(plainBytes);
                 }
@@ -68,5 +84,48 @@ namespace YetaWF.Core.Security {
                 throw new InternalError("Failure decrypting - {0}", ex.Message);
             }
         }
+#if NETCOREAPP
+        // https://github.com/dotnet/corefx/issues/23686 fucking snowflakes
+
+        public static void TranslateFromXmlString(this RSA rsa, string xmlString) {
+            RSAParameters parameters = new RSAParameters();
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlString);
+
+            if (xmlDoc.DocumentElement.Name.Equals("RSAKeyValue")) {
+                foreach (XmlNode node in xmlDoc.DocumentElement.ChildNodes) {
+                    switch (node.Name) {
+                        case "Modulus": parameters.Modulus = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "Exponent": parameters.Exponent = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "P": parameters.P = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "Q": parameters.Q = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "DP": parameters.DP = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "DQ": parameters.DQ = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "InverseQ": parameters.InverseQ = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                        case "D": parameters.D = (string.IsNullOrEmpty(node.InnerText) ? null : Convert.FromBase64String(node.InnerText)); break;
+                    }
+                }
+            } else {
+                throw new Exception("Invalid XML RSA key.");
+            }
+            rsa.ImportParameters(parameters);
+        }
+        public static string TranslateToXmlString(this RSA rsa, bool includePrivateParameters) {
+            RSAParameters parameters = rsa.ExportParameters(includePrivateParameters);
+
+            return string.Format("<RSAKeyValue><Modulus>{0}</Modulus><Exponent>{1}</Exponent><P>{2}</P><Q>{3}</Q><DP>{4}</DP><DQ>{5}</DQ><InverseQ>{6}</InverseQ><D>{7}</D></RSAKeyValue>",
+                  parameters.Modulus != null ? Convert.ToBase64String(parameters.Modulus) : null,
+                  parameters.Exponent != null ? Convert.ToBase64String(parameters.Exponent) : null,
+                  parameters.P != null ? Convert.ToBase64String(parameters.P) : null,
+                  parameters.Q != null ? Convert.ToBase64String(parameters.Q) : null,
+                  parameters.DP != null ? Convert.ToBase64String(parameters.DP) : null,
+                  parameters.DQ != null ? Convert.ToBase64String(parameters.DQ) : null,
+                  parameters.InverseQ != null ? Convert.ToBase64String(parameters.InverseQ) : null,
+                  parameters.D != null ? Convert.ToBase64String(parameters.D) : null);
+        }
+#else
+#endif
     }
 }
+
