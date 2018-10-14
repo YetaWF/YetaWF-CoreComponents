b01d6cf73a56497fde558a58b66e5fe40dab57e8
YetaWF <mikevdm@YetaWF.com>
2018-10-14 07:49:28 -0400

    child element remove polyfill (grid)

--- Core/Addons/_Addons/Basics/BasicsProto.js
+++ Core/Addons/_Addons/Basics/BasicsProto.js
@@ -46,6 +46,24 @@ if (!Element.prototype.matches) {
         };
 }
 
+// from:https://github.com/jserz/js_piece/blob/master/DOM/ChildNode/remove()/remove().md
+(function (arr) {
+    arr.forEach(function (item) {
+        if (item.hasOwnProperty('remove')) {
+            return;
+        }
+        Object.defineProperty(item, 'remove', {
+            configurable: true,
+            enumerable: true,
+            writable: true,
+            value: function remove() {
+                if (this.parentNode !== null)
+                    this.parentNode.removeChild(this);
+            }
+        });
+    });
+})([Element.prototype, CharacterData.prototype, DocumentType.prototype]);
+
 if (!Number.MAX_SAFE_INTEGER) {
     Number.MAX_SAFE_INTEGER = Math.pow(2, 53) - 1; // 9007199254740991
 }
