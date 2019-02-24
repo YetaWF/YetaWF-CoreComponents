d35b8e09cc3a1ee7a6296be9a98aae6e429397c4
YetaWF <mikevdm@YetaWF.com>
2019-02-24 10:17:41 -0500

    comments

--- Core/DataProvider/IDataProvider.cs
+++ Core/DataProvider/IDataProvider.cs
@@ -40,12 +40,20 @@ namespace YetaWF.Core.DataProvider {
         public bool More { get; set; }
     }
 
+    /// <summary>
+    /// This interface is implemented by low-level data providers to support transactions that can be committed, saving all updates, or aborted to abandon all updates.
+    /// </summary>
     public interface IDataProviderTransactions {
         DataProviderTransaction StartTransaction();
         Task CommitTransactionAsync();
         void AbortTransaction();
     }
 
+    /// <summary>
+    /// This interface is implemented by low-level data providers that offer access to record-based data with one primary key, without record identity.
+    /// </summary>
+    /// <typeparam name="KEYTYPE">The type of the primary key property.</typeparam>
+    /// <typeparam name="OBJTYPE">The type of the object (one record) in the dataset.</typeparam>
     public interface IDataProvider<KEYTYPE, OBJTYPE> {
 
         DataProviderTransaction StartTransaction();
@@ -59,16 +67,88 @@ namespace YetaWF.Core.DataProvider {
 
         Task<OBJTYPE> GetAsync(KEYTYPE key); // returns null if not found
         Task<OBJTYPE> GetOneRecordAsync(List<DataProviderFilterInfo> filters, List<JoinData> Joins = null); // returns null if not found
-        Task<DataProviderGetRecords<OBJTYPE>> GetRecordsAsync(int skip, int take, List<DataProviderSortInfo> sort, List<DataProviderFilterInfo> filters, List<JoinData> Joins = null);        
+        Task<DataProviderGetRecords<OBJTYPE>> GetRecordsAsync(int skip, int take, List<DataProviderSortInfo> sort, List<DataProviderFilterInfo> filters, List<JoinData> Joins = null);
 
+        /// <summary>
+        /// Returns whether the data provider is installed and available.
+        /// </summary>
+        /// <returns>true if the data provider is installed and available, false otherwise.</returns>
         Task<bool> IsInstalledAsync();
+        /// <summary>
+        /// Installs all data models (files, tables, etc.) for the data provider.
+        /// </summary>
+        /// <param name="errorList">A collection of error strings in user displayable format.</param>
+        /// <returns>true if the models were created successfully, false otherwise.
+        /// If the models could not be created, <paramref name="errorList"/> contains the reason for the failure.</returns>
+        /// <remarks>
+        /// While a package is installed, all data models are installed by calling the InstallModelAsync method.</remarks>
         Task<bool> InstallModelAsync(List<string> errorList);
+        /// <summary>
+        /// Uninstalls all data models (files, tables, etc.) for the data provider.
+        /// </summary>
+        /// <param name="errorList">A collection of error strings in user displayable format.</param>
+        /// <returns>true if the models were removed successfully, false otherwise.
+        /// If the models could not be removed, <paramref name="errorList"/> contains the reason for the failure.</returns>
+        /// <remarks>
+        /// While a package is uninstalled, all data models are uninstalled by calling the UninstallModelAsync method.</remarks>
         Task<bool> UninstallModelAsync(List<string> errorList);
+        /// <summary>
+        /// Adds data for a new site.
+        /// </summary>
+        /// <remarks>
+        /// When a new site is created the AddSiteDataAsync method is called for all data providers.
+        /// Data providers can then add site-specific data as the new site is added.</remarks>
         Task AddSiteDataAsync();
+        /// <summary>
+        /// Removes data when a site is deleted.
+        /// </summary>
+        /// <remarks>
+        /// When a site is deleted the RemoveSiteDataAsync method is called for all data providers.
+        /// Data providers can then remove site-specific data as the site is removed.</remarks>
         Task RemoveSiteDataAsync();
+        /// <summary>
+        /// Imports data into the data provider.
+        /// </summary>
+        /// <param name="chunk">The zero-based chunk number as data is imported. The first call when importing begins specifies 0 as chunk number.</param>
+        /// <param name="fileList">A collection of files to be imported. Files are automatically imported, so the data provider doesn't have to process this collection.</param>
+        /// <param name="obj">The data to be imported.</param>
+        /// <remarks>
+        /// The ImportChunkAsync method is called to import data for site restores, page and module imports.
+        ///
+        /// When a data provider is called to import data, it is called repeatedly until no more data is available.
+        /// Each time it is called, it is expected to import the chunk of data defined by <paramref name="obj"/>.
+        /// Each time ImportChunkAsync method is called, the zero-based chunk number <paramref name="chunk"/> is incremented.
+        ///
+        /// The <paramref name="obj"/> parameter is provided without type but should be cast to
+        /// SerializableList&lt;OBJTYPE&gt; as it is a collection of records to import. All records in the collection must be imported.
+        /// </remarks>
         Task ImportChunkAsync(int chunk, SerializableList<SerializableFile> fileList, object obj);
+        /// <summary>
+        /// Exports data from the data provider.
+        /// </summary>
+        /// <param name="chunk">The zero-based chunk number as data is exported. The first call when exporting begins specifies 0 as chunk number.</param>
+        /// <param name="fileList">A collection of files. The data provider can add files to be exported to this collection when ExportChunkAsync is called.</param>
+        /// <returns>Returns a DataProviderExportChunk object describing the data exported.</returns>
+        /// <remarks>
+        /// The ExportChunkAsync method is called to export data for site backups, page and module exports.
+        ///
+        /// When a data provider is called to export data, it is called repeatedly until YetaWF.Core.DataProvider.DataProviderExportChunk.More is returned as false.
+        /// Each time it is called, it is expected to export a chunk of data. The amount of data, i.e., the chunk size, is determined by the data provider.
+        ///
+        /// Each time ExportChunkAsync method is called, the zero-based chunk number <paramref name="chunk"/> is incremented.
+        /// The data provider returns data in an instance of the DataProviderExportChunk object.
+        ///
+        /// Files to be exported can be added to the <paramref name="fileList"/> collection.
+        /// Only data records need to be added to the returned DataProviderExportChunk object.
+        /// </remarks>
         Task<DataProviderExportChunk> ExportChunkAsync(int chunk, SerializableList<SerializableFile> fileList);
     }
+    /// <summary>
+    /// This interface is implemented by low-level data providers that offer access to record-based data with up to two primary keys and a record identity.
+    /// </summary>
+    /// <typeparam name="KEYTYPE">The type of the primary key property.</typeparam>
+    /// <typeparam name="KEY2TYPE">The type of the second primary key property. If only one key is used, specify "object".</typeparam>
+    /// <typeparam name="OBJTYPE">The type of the object (one record) in the dataset.</typeparam>
     public interface IDataProviderIdentity<KEYTYPE, KEY2TYPE, OBJTYPE> {
 
         DataProviderTransaction StartTransaction();
@@ -87,12 +167,78 @@ namespace YetaWF.Core.DataProvider {
         Task<OBJTYPE> GetOneRecordAsync(List<DataProviderFilterInfo> filters, List<JoinData> Joins = null); // returns null if not found
         Task<DataProviderGetRecords<OBJTYPE>> GetRecordsAsync(int skip, int take, List<DataProviderSortInfo> sort, List<DataProviderFilterInfo> filters, List<JoinData> Joins = null);
 
+        /// <summary>
+        /// Returns whether the data provider is installed and available.
+        /// </summary>
+        /// <returns>true if the data provider is installed and available, false otherwise.</returns>
         Task<bool> IsInstalledAsync();
+        /// <summary>
+        /// Installs all data models (files, tables, etc.) for the data provider.
+        /// </summary>
+        /// <param name="errorList">A collection of error strings in user displayable format.</param>
+        /// <returns>true if the models were created successfully, false otherwise.
+        /// If the models could not be created, <paramref name="errorList"/> contains the reason for the failure.</returns>
+        /// <remarks>
+        /// While a package is installed, all data models are installed by calling the InstallModelAsync method.</remarks>
         Task<bool> InstallModelAsync(List<string> errorList);
+        /// <summary>
+        /// Uninstalls all data models (files, tables, etc.) for the data provider.
+        /// </summary>
+        /// <param name="errorList">A collection of error strings in user displayable format.</param>
+        /// <returns>true if the models were removed successfully, false otherwise.
+        /// If the models could not be removed, <paramref name="errorList"/> contains the reason for the failure.</returns>
+        /// <remarks>
+        /// While a package is uninstalled, all data models are uninstalled by calling the UninstallModelAsync method.</remarks>
         Task<bool> UninstallModelAsync(List<string> errorList);
+        /// <summary>
+        /// Adds data for a new site.
+        /// </summary>
+        /// <remarks>
+        /// When a new site is created the AddSiteDataAsync method is called for all data providers.
+        /// Data providers can then add site-specific data as the new site is added.</remarks>
         Task AddSiteDataAsync();
+        /// <summary>
+        /// Removes data when a site is deleted.
+        /// </summary>
+        /// <remarks>
+        /// When a site is deleted the RemoveSiteDataAsync method is called for all data providers.
+        /// Data providers can then remove site-specific data as the site is removed.</remarks>
         Task RemoveSiteDataAsync();
+        /// <summary>
+        /// Imports data into the data provider.
+        /// </summary>
+        /// <param name="chunk">The zero-based chunk number as data is imported. The first call when importing begins specifies 0 as chunk number.</param>
+        /// <param name="fileList">A collection of files to be imported. Files are automatically imported, so the data provider doesn't have to process this collection.</param>
+        /// <param name="obj">The data to be imported.</param>
+        /// <remarks>
+        /// The ImportChunkAsync method is called to import data for site restores, page and module imports.
+        ///
+        /// When a data provider is called to import data, it is called repeatedly until no more data is available.
+        /// Each time it is called, it is expected to import the chunk of data defined by <paramref name="obj"/>.
+        /// Each time ImportChunkAsync method is called, the zero-based chunk number <paramref name="chunk"/> is incremented.
+        ///
+        /// The <paramref name="obj"/> parameter is provided without type but should be cast to
+        /// SerializableList&lt;OBJTYPE&gt; as it is a collection of records to import. All records in the collection must be imported.
+        /// </remarks>
         Task ImportChunkAsync(int chunk, SerializableList<SerializableFile> fileList, object obj);
+        /// <summary>
+        /// Exports data from the data provider.
+        /// </summary>
+        /// <param name="chunk">The zero-based chunk number as data is exported. The first call when exporting begins specifies 0 as chunk number.</param>
+        /// <param name="fileList">A collection of files. The data provider can add files to be exported to this collection when ExportChunkAsync is called.</param>
+        /// <returns>Returns a DataProviderExportChunk object describing the data exported.</returns>
+        /// <remarks>
+        /// The ExportChunkAsync method is called to export data for site backups, page and module exports.
+        ///
+        /// When a data provider is called to export data, it is called repeatedly until YetaWF.Core.DataProvider.DataProviderExportChunk.More is returned as false.
+        /// Each time it is called, it is expected to export a chunk of data. The amount of data, i.e., the chunk size, is determined by the data provider.
+        ///
+        /// Each time ExportChunkAsync method is called, the zero-based chunk number <paramref name="chunk"/> is incremented.
+        /// The data provider returns data in an instance of the DataProviderExportChunk object.
+        ///
+        /// Files to be exported can be added to the <paramref name="fileList"/> collection.
+        /// Only data records need to be added to the returned DataProviderExportChunk object.
+        /// </remarks>
         Task<DataProviderExportChunk> ExportChunkAsync(int chunk, SerializableList<SerializableFile> fileList);
     }
 }
--- Core/IO/InstallableModel.cs
+++ Core/IO/InstallableModel.cs
@@ -9,48 +9,115 @@ using YetaWF.Core.Serializers;
 
 namespace YetaWF.Core.IO {
 
+    /// <summary>
+    /// This interface is implemented by data providers that support installable data.
+    /// </summary>
     public interface IInstallableModel {
 
         /// <summary>
-        /// Returns whether the model is installed (assemblies may be present but the data source is not available)
+        /// Returns whether the data provider is installed and available.
         /// </summary>
+        /// <returns>true if the data provider is installed and available, false otherwise.</returns>
         Task<bool> IsInstalledAsync();
 
         /// <summary>
-        /// Installs all required files/folders/SQL data.
+        /// Installs all data models (files, tables, etc.) for the data provider.
         /// </summary>
+        /// <param name="errorList">A collection of error strings in user displayable format.</param>
+        /// <returns>true if the models were created successfully, false otherwise.
+        /// If the models could not be created, <paramref name="errorList"/> contains the reason for the failure.</returns>
+        /// <remarks>
+        /// While a package is installed, all data models are installed by calling the InstallModelAsync method.</remarks>
         Task<bool> InstallModelAsync(List<string> errorList);
 
         /// <summary>
-        /// Uninstalls all files/folders/SQL data managed by this model.
+        /// Uninstalls all data models (files, tables, etc.) for the data provider.
         /// </summary>
+        /// <param name="errorList">A collection of error strings in user displayable format.</param>
+        /// <returns>true if the models were removed successfully, false otherwise.
+        /// If the models could not be removed, <paramref name="errorList"/> contains the reason for the failure.</returns>
+        /// <remarks>
+        /// While a package is uninstalled, all data models are uninstalled by calling the UninstallModelAsync method.</remarks>
         Task<bool> UninstallModelAsync(List<string> errorList);
 
         /// <summary>
-        /// Add site-specific data for this model
+        /// Adds data for a new site.
         /// </summary>
+        /// <remarks>
+        /// When a new site is created the AddSiteDataAsync method is called for all data providers.
+        /// Data providers can then add site-specific data as the new site is added.</remarks>
         Task AddSiteDataAsync();
 
         /// <summary>
-        /// Removes site-specific data for this model
+        /// Removes data when a site is deleted.
         /// </summary>
+        /// <remarks>
+        /// When a site is deleted the RemoveSiteDataAsync method is called for all data providers.
+        /// Data providers can then remove site-specific data as the site is removed.</remarks>
         Task RemoveSiteDataAsync();
 
         /// <summary>
-        /// Exports data this model implements (in model defined chunk increments (0..n))
+        /// Exports data from the data provider.
         /// </summary>
+        /// <param name="chunk">The zero-based chunk number as data is exported. The first call when exporting begins specifies 0 as chunk number.</param>
+        /// <param name="fileList">A collection of files. The data provider can add files to be exported to this collection when ExportChunkAsync is called.</param>
+        /// <returns>Returns a DataProviderExportChunk object describing the data exported.</returns>
+        /// <remarks>
+        /// The ExportChunkAsync method is called to export data for site backups, page and module exports.
+        ///
+        /// When a data provider is called to export data, it is called repeatedly until YetaWF.Core.DataProvider.DataProviderExportChunk.More is returned as false.
+        /// Each time it is called, it is expected to export a chunk of data. The amount of data, i.e., the chunk size, is determined by the data provider.
+        ///
+        /// Each time ExportChunkAsync method is called, the zero-based chunk number <paramref name="chunk"/> is incremented.
+        /// The data provider returns data in an instance of the DataProviderExportChunk object.
+        ///
+        /// Files to be exported can be added to the <paramref name="fileList"/> collection.
+        /// Only data records need to be added to the returned DataProviderExportChunk object.
+        /// </remarks>
         Task<DataProviderExportChunk> ExportChunkAsync(int chunk, SerializableList<SerializableFile> fileList);
 
         /// <summary>
-        /// Imports all data this model implements (in model defined chunk increments (0..n))
+        /// Imports data into the data provider.
         /// </summary>
+        /// <param name="chunk">The zero-based chunk number as data is imported. The first call when importing begins specifies 0 as chunk number.</param>
+        /// <param name="fileList">A collection of files to be imported. Files are automatically imported, so the data provider doesn't have to process this collection.</param>
+        /// <param name="obj">The data to be imported.</param>
+        /// <remarks>
+        /// The ImportChunkAsync method is called to import data for site restores, page and module imports.
+        ///
+        /// When a data provider is called to import data, it is called repeatedly until no more data is available.
+        /// Each time it is called, it is expected to import the chunk of data defined by <paramref name="obj"/>.
+        /// Each time ImportChunkAsync method is called, the zero-based chunk number <paramref name="chunk"/> is incremented.
+        ///
+        /// The <paramref name="obj"/> parameter is provided without type but should be cast to
+        /// SerializableList&lt;OBJTYPE&gt; as it is a collection of records to import. All records in the collection must be imported.
+        /// </remarks>
         Task ImportChunkAsync(int chunk, SerializableList<SerializableFile> fileList, object obj);
 
         /// <summary>
-        /// Translate all data for this model into the specified language.
+        /// Called to translate the data managed by the data provider to another language.
         /// </summary>
+        /// <param name="language">The target language (see LanguageSettings.json).</param>
+        /// <param name="isHtml">A function that can be called by the data provider to test whether a string contains HTML.</param>
+        /// <param name="translateStringsAsync">A method that can be called to translate a collection of simple strings into the new language. A simple string does not contain HTML or newline characters.</param>
+        /// <param name="translateComplexStringAsync">A method that can be called to translate a collection of complex strings into the new language. A complex string can contain HTML and newline characters.</param>
+        /// <remarks>
+        /// The data provider has to retrieve all records and translate them as needed using the
+        /// provided <paramref name="translateStringsAsync"/> and <paramref name="translateComplexStringAsync"/> methods, and save the translated data.
+        ///
+        /// The YetaWF.Core.Models.ObjectSupport.TranslateObject method can be used to translate all MultiString instances.
+        ///
+        /// The translated data should be stored separately from the default language (except MultiString, which is part of the record). Using the <paramref name="language"/> parameter, a different folder should be used to store the translated data.
+        /// </remarks>
         Task LocalizeModelAsync(string language, Func<string, bool> isHtml, Func<List<string>, Task<List<string>>> translateStringsAsync, Func<string, Task<string>> translateComplexStringAsync);
     }
+    /// <summary>
+    /// This interface is implemented by data providers that support upgrading data from one YetaWF version to another.
+    /// </summary>
+    /// <remarks>
+    /// Typically upgrading doesn't require any special handling other than adding/removing columns, which is normally automatically handled by the data provider.
+    /// In rare cases, the data has to be converted to a completely new format, which can be done using the IInstallableModel2 interface.
+    /// </remarks>
     public interface IInstallableModel2 {
         /// <summary>
         /// Upgrades all required files/folders/SQL data from lastSeenVersion to current package version.
