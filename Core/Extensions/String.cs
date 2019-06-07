7e95f222cffa307dfcccea59261ada1a0dbe9f67
YetaWF <mikevdm@YetaWF.com>
2019-06-07 11:45:10 -0400

    removelast comma stringbuilder

--- Core/Extensions/String.cs
+++ Core/Extensions/String.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace YetaWF.Core.Extensions {
 
-    public static class Extensions {
+    public static partial class Extensions {
 
         public static string Truncate(this string text, int maxLength) {
             if (string.IsNullOrEmpty(text)) return text;
