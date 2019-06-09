0fb6c63b72ee07744799ced1ec1a8522b83a17a4
YetaWF <mikevdm@YetaWF.com>
2019-06-09 11:58:53 -0400

    StartBySiteDefinition

--- Core/Startup/StartupBatch.cs
+++ Core/Startup/StartupBatch.cs
@@ -24,6 +24,7 @@ namespace YetaWF.Core.Support {
         /// <param name="siteDomain">The domain name used to access data. This must be an existing domain with a YetaWF site and AppSettings.json must contain data provider information.</param>
         /// <remarks>
         /// The Start method makes all settings from AppSettings.json available. In release builds, AppSettings.PROD.json is used instead.
+        /// In a docker container, AppSettings.Docker.json is used.
         ///
         /// A LanguageSettings.json file must be present defining all languages used by the specified site <paramref name="siteDomain"/> (a copy of the LanguageSettings.json file used for the website).
         ///
@@ -51,6 +52,7 @@ namespace YetaWF.Core.Support {
         /// <param name="siteIdentity">The domain's site identity used to access data. This must be an existing domain with a YetaWF site and AppSettings.json must contain data provider information.</param>
         /// <remarks>
         /// The Start method makes all settings from AppSettings.json available. In release builds, AppSettings.PROD.json is used instead.
+        /// In a docker container, AppSettings.Docker.json is used.
         ///
         /// A LanguageSettings.json file must be present defining all languages used by the specified site <paramref name="siteIdentity"/> (a copy of the LanguageSettings.json file used for the website).
         ///
@@ -69,6 +71,37 @@ namespace YetaWF.Core.Support {
             YetaWFManager.Manager.CurrentSite = new SiteDefinition() {
                 Identity = siteIdentity
             };
+
+            YetaWF.Core.Support.Startup.Started = true;
+        }
+
+        /// <summary>
+        /// Called to initialize a console application so it can use all of YetaWF's services, including data providers, caching, etc.
+        /// </summary>
+        /// <param name="baseDirectory">The base folder where the executable and all assemblies for the console application are located.</param>
+        /// <param name="filePath">The path and file name of a file containing json describing the site to use. Files with json information are saved in the website's
+        ///  ./Website/Data/Sites folder whenever site settings are saved.</param>
+        /// <remarks>
+        /// The Start method makes all settings from AppSettings.json available. In release builds, AppSettings.PROD.json is used instead.
+        /// In a docker container, AppSettings.Docker.json is used.
+        ///
+        /// A LanguageSettings.json file must be present defining all languages used by the specified site <paramref name="filePath"/> (a copy of the LanguageSettings.json file used for the website).
+        ///
+        /// The console application must have references to the YetaWF.Core and YetaWF.Caching. The YetaWF.SiteProperties package is not required.
+        /// Using this method to start, limited services can be used.
+        /// Additional references must be added for services used by the console application.
+        ///
+        /// Because all YetaWF services are available, all data providers and config settings can be accessed (and modified).
+        /// Many data providers use site specific data. The data for the specified site <paramref name="filePath"/> is used.
+        /// </remarks>
+        public static void StartBySiteDefinitionFile(string baseDirectory, string filePath) {
+
+            Start(baseDirectory);
+
+            // Set up specific site to use
+            string siteDefJson = File.ReadAllText(filePath);
+            SiteDefinition site = YetaWFManager.JsonDeserialize<SiteDefinition>(siteDefJson);
+            YetaWFManager.Manager.CurrentSite = site;
 
             YetaWF.Core.Support.Startup.Started = true;
         }
