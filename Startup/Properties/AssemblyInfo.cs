71ebfa2308ea2ff13fcd4cea1ca3dc5b162cf194
YetaWF <mikevdm@YetaWF.com>
2019-10-03 21:28:35 -0400

    startup

---
+++ Startup/Properties/AssemblyInfo.cs
@@ -0,0 +1,27 @@
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+using YetaWF.Core.Packages;
+using YetaWF.PackageAttributes;
+
+[assembly: AssemblyTitle("Startup Support")]
+[assembly: AssemblyDescription("YetaWF Startup Support")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Softel vdm, Inc.")]
+[assembly: AssemblyCopyright("Copyright © 2019 - Softel vdm, Inc.")]
+[assembly: AssemblyProduct("Startup")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("5.0.0.0")]
+
+[assembly: Package(PackageTypeEnum.CoreAssembly, "YetaWF")]
+[assembly: ServiceLevel(ServiceLevelEnum.Core)]
+
+[assembly: PackageInfo("https://YetaWF.com/UpdateServer",
+    "https://yetawf.com/Documentation/YetaWF/Startup",
+    "https://YetaWF.com/Documentation/YetaWF/Support",
+    "https://yetawf.com/Documentation/YetaWF/Startup#Release%20Notice",
+    "https://yetawf.com/Documentation/YetaWF/Startup#License")]
+
