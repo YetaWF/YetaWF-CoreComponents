224df4a23a0f9fe224ae288e3d97e91d82b72ada
Mike <mikevdm@mikevdm.com>
2019-11-18 12:45:18 -0500

    add IsConnectedToInternet support

---
+++ ConsoleStartup/Support.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace YetaWF.Core.ConsoleStartup.Support {
+
+    /// <summary>
+    /// Class implementing standard support functions for console applications.
+    /// </summary>
+    public static class Support {
+
+        /// <summary>
+        /// Tests whether the application can access the Internet.
+        /// </summary>
+        /// <returns></returns>
+        public static bool IsConnectedToInternet() {
+            try {
+                Ping myPing = new Ping();
+                String host = "8.8.8.8";
+                byte[] buffer = new byte[32];
+                int timeout = 1000;
+                PingOptions pingOptions = new PingOptions();
+                PingReply reply = myPing.Send(host, timeout, buffer, pingOptions);
+                return (reply.Status == IPStatus.Success);
+            } catch { }
+            return false;
+        }
+    }
+}
