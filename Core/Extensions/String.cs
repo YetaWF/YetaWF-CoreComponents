39da60b5a537533a4755c3d92c6b1d5facab8e54
Mike <mikevdm@mikevdm.com>
2020-02-15 10:51:42 -0500

    removetrailingslash

--- Core/Extensions/String.cs
+++ Core/Extensions/String.cs
@@ -112,6 +112,14 @@ namespace YetaWF.Core.Extensions {
             else
                 return text;
         }
+        public static string RemoveTrailingSlash(this string text) {
+            if (string.IsNullOrWhiteSpace(text))
+                return text;
+            if (text.EndsWith("/"))
+                return text.Substring(0, text.Length - 1);
+            else
+                return text;
+        }
         public static string AddUrlCacheBuster(this string text, string cacheBuster) {
             if (string.IsNullOrWhiteSpace(cacheBuster)) return "";
             if (text == null) return "";
