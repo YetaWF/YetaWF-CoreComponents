65a158e37ad8a149a2f2a24d82397b34809d5674
Mike <mikevdm@mikevdm.com>
2020-02-27 17:00:38 -0500

    areaname constant

--- Core/Startup/MVC6/LetsEncrypt.cs
+++ Core/Startup/MVC6/LetsEncrypt.cs
@@ -17,27 +17,29 @@ namespace YetaWF2.LetsEncrypt {
 
     public static class LetsEncrypt {
 
+        private const string AREANAME = "LetsEncrypt";
+
         public static void AddLetsEncrypt(this IServiceCollection services) {
 
             // https://github.com/ffMathy/FluffySpoon.AspNet.LetsEncrypt
-            string domains = WebConfigHelper.GetValue<string>("LetsEncrypt", "Domains", null, Package: false);
+            string domains = WebConfigHelper.GetValue<string>(AREANAME, "Domains", null, Package: false);
             if (!string.IsNullOrWhiteSpace(domains)) {
                 services.AddFluffySpoonLetsEncrypt(new LetsEncryptOptions() {
-                    Email = WebConfigHelper.GetValue<string>("LetsEncrypt", "Email", Package: false, Required: true), // LetsEncrypt will send you an e-mail here when the certificate is about to expire
-                    UseStaging = WebConfigHelper.GetValue<bool>("LetsEncrypt", "Staging", Package: false, Required: true), // false for production
-                    Domains = WebConfigHelper.GetValue<string>("LetsEncrypt", "Domains", Package: false, Required: true).Split(new char[] { ',' }),
-                    TimeUntilExpiryBeforeRenewal = TimeSpan.FromDays(WebConfigHelper.GetValue<int>("LetsEncrypt", "TimeUntilExpiryBeforeRenewal", 30, Package: false)), // renew automatically 30 days before expiry
-                    TimeAfterIssueDateBeforeRenewal = TimeSpan.FromDays(WebConfigHelper.GetValue<int>("LetsEncrypt", "TimeAfterIssueDateBeforeRenewal", 7, Package: false)), // renew automatically 7 days after the last certificate was issued
+                    Email = WebConfigHelper.GetValue<string>(AREANAME, "Email", Package: false, Required: true), // LetsEncrypt will send you an e-mail here when the certificate is about to expire
+                    UseStaging = WebConfigHelper.GetValue<bool>(AREANAME, "Staging", Package: false, Required: true), // false for production
+                    Domains = WebConfigHelper.GetValue<string>(AREANAME, "Domains", Package: false, Required: true).Split(new char[] { ',' }),
+                    TimeUntilExpiryBeforeRenewal = TimeSpan.FromDays(WebConfigHelper.GetValue<int>(AREANAME, "TimeUntilExpiryBeforeRenewal", 30, Package: false)), // renew automatically 30 days before expiry
+                    TimeAfterIssueDateBeforeRenewal = TimeSpan.FromDays(WebConfigHelper.GetValue<int>(AREANAME, "TimeAfterIssueDateBeforeRenewal", 7, Package: false)), // renew automatically 7 days after the last certificate was issued
                     CertificateSigningRequest = new CsrInfo() // these are your certificate details
                     {
-                        CountryName = WebConfigHelper.GetValue<string>("LetsEncrypt", "CountryName", "United States", Package: false, Required: true),
-                        Locality = WebConfigHelper.GetValue<string>("LetsEncrypt", "Locality", "US", Package: false, Required: true),
-                        Organization = WebConfigHelper.GetValue<string>("LetsEncrypt", "Organization", Package: false, Required: true),
-                        OrganizationUnit = WebConfigHelper.GetValue<string>("LetsEncrypt", "OrganizationUnit", Package: false, Required: true),
-                        State = WebConfigHelper.GetValue<string>("LetsEncrypt", "State", Package: false, Required: true),
+                        CountryName = WebConfigHelper.GetValue<string>(AREANAME, "CountryName", "United States", Package: false, Required: true),
+                        Locality = WebConfigHelper.GetValue<string>(AREANAME, "Locality", "US", Package: false, Required: true),
+                        Organization = WebConfigHelper.GetValue<string>(AREANAME, "Organization", Package: false, Required: true),
+                        OrganizationUnit = WebConfigHelper.GetValue<string>(AREANAME, "OrganizationUnit", Package: false, Required: true),
+                        State = WebConfigHelper.GetValue<string>(AREANAME, "State", Package: false, Required: true),
                     }
                 });
-                string certFolder = WebConfigHelper.GetValue<string>("LetsEncrypt", "Certs", Globals.DataFolder, Package: false);
+                string certFolder = WebConfigHelper.GetValue<string>(AREANAME, "Certs", Globals.DataFolder, Package: false);
                 string certPath = Path.Combine(YetaWFManager.RootFolderWebProject, certFolder);
                 services.AddFluffySpoonLetsEncryptCertificatePersistence(
                     (key, bytes) => {
@@ -59,7 +61,7 @@ namespace YetaWF2.LetsEncrypt {
 
         }
         public static void UseLetsEncrypt (this IApplicationBuilder app) {
-            string domains = WebConfigHelper.GetValue<string>("LetsEncrypt", "Domains", null, Package: false);
+            string domains = WebConfigHelper.GetValue<string>(AREANAME, "Domains", null, Package: false);
             if (!string.IsNullOrWhiteSpace(domains)) {
                 app.UseFluffySpoonLetsEncrypt();
             }
