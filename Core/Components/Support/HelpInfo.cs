2cc8bf1778eed7727a654c5cbde1823368b62a82
Mike <mikevdm@mikevdm.com>
2020-05-23 08:44:09 -0400

    helpinfo component localization/caching

--- Core/Components/Support/HelpInfo.cs
+++ Core/Components/Support/HelpInfo.cs
@@ -4,8 +4,30 @@ using YetaWF.Core.Packages;
 
 namespace YetaWF.Core.Components {
 
+    /// <summary>
+    /// Definitions for the HelpInfo component used to render a help file.
+    /// The help file contains HTML and is rendered as-is.
+    /// </summary>
     public class HelpInfoDefinition {
+        /// <summary>
+        /// The package owning the help file.
+        /// </summary>
         public Package Package { get; set; }
+        /// <summary>
+        /// The name of the help file (without path or file extension.
+        /// </summary>
         public string Name { get; set; }
+        /// <summary>
+        /// Defines whether the small object cache is used to cache the help file. Use for small help files only.
+        /// Any help files over 1K bytes of data are not cached.
+        /// </summary>
+        public bool UseCache { get; set; }
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public HelpInfoDefinition() {
+            UseCache = true;
+        }
     }
 }
