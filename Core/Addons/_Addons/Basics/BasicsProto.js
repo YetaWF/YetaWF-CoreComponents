108e8005395411ada7543a806c278c1c55ba1c9a
Mike <mikevdm@mikevdm.com>
2020-10-12 12:33:52 -0400

    add customevent (ie11)

--- Core/Addons/_Addons/Basics/BasicsProto.js
+++ Core/Addons/_Addons/Basics/BasicsProto.js
@@ -240,6 +240,21 @@ if (!Array.from) {
     }());
 }
 
+// https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent/CustomEvent
+(function () {
+
+    if (typeof window.CustomEvent === "function") return false;
+
+    function CustomEvent(event, params) {
+        params = params || { bubbles: false, cancelable: false, detail: null };
+        var evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
+    }
+
+    window.CustomEvent = CustomEvent;
+})();
+
 // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/indexOf
 
 // This version tries to optimize by only checking for "in" when looking for undefined and
