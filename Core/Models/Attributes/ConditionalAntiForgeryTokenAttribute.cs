ad6265791bc03f5af61125916a1a167661e3d3a0
Mike <mikevdm@mikevdm.com>
2020-11-18 06:54:31 -0500

    enable antiforgery everywhere (can no longer use on static pages)

--- Core/Models/Attributes/ConditionalAntiForgeryTokenAttribute.cs
+++ Core/Models/Attributes/ConditionalAntiForgeryTokenAttribute.cs
@@ -1,14 +1,12 @@
 ﻿/* Copyright © 2020 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
-using System;
-using YetaWF.Core.Localize;
-using YetaWF.Core.Support;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Antiforgery;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
 
 namespace YetaWF.Core.Models.Attributes {
 
@@ -20,16 +18,11 @@ namespace YetaWF.Core.Models.Attributes {
     /// Antiforgery token validation.
     /// </summary>
     /// <remarks>
-    /// If a site enables static pages, anti-forgery tokens are not validated for anonymous users (ONLY). This exposes no
-    /// security concerns as anti-forgery tokens are intended to prevent Cross-Site Request Forgery (CSRF) for authenticated users (i.e., NOT anonymous users).
+    /// An antiforgery token cannot be used on a static page.
     ///
-    /// If static pages are not enabled for a site, anti-forgery tokens are used and validated in all cases (anonymous or authenticated users).
-    /// </remarks>
+    /// There is nothing conditional about this despite the name. This is a legacy remnant.</remarks>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
     public class ConditionalAntiForgeryTokenAttribute : Attribute, IFilterFactory, IOrderedFilter {
-        private static string __ResStr(string name, string defaultValue, params object?[] parms) { return ResourceAccess.GetResourceString(typeof(Resources), name, defaultValue, parms); }
-
-        private YetaWFManager Manager { get { return YetaWFManager.Manager; } }
 
         public int Order { get; set; } = 1000;
 
@@ -44,20 +37,15 @@ namespace YetaWF.Core.Models.Attributes {
 
         private readonly IAntiforgery _antiforgery;
 
-        private YetaWFManager Manager { get { return YetaWFManager.Manager; } }
-
         public ConditionalAntiForgeryTokenFilter(IAntiforgery antiforgery) {
-            if (antiforgery == null) {
+            if (antiforgery == null)
                 throw new ArgumentNullException(nameof(antiforgery));
-            }
-
             _antiforgery = antiforgery;
         }
 
         public async Task OnAuthorizationAsync(AuthorizationFilterContext context) {
-            if (context == null) {
+            if (context == null)
                 throw new ArgumentNullException(nameof(context));
-            }
 
             if (ShouldValidate(context)) {
                 try {
@@ -69,8 +57,6 @@ namespace YetaWF.Core.Models.Attributes {
         }
 
         protected virtual bool ShouldValidate(AuthorizationFilterContext context) {
-            if (!Manager.HaveUser && Manager.CurrentSite.StaticPages && Manager.HostUsed.ToLower() == Manager.CurrentSite.SiteDomain.ToLower())
-                return false; // don't validate AntiForgeryToken when we use static pages and have an anonymous user
             return true;
         }
     }
