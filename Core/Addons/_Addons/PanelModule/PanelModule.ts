b1fad1b53754b8f9b8be6e2eb9da6dbef9d56b06
Mike <mikevdm@mikevdm.com>
2021-02-07 22:05:14 -0500

    copyright

--- Core/Addons/_Addons/PanelModule/PanelModule.ts
+++ Core/Addons/_Addons/PanelModule/PanelModule.ts
@@ -1,4 +1,4 @@
-/* Copyright © 2021 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/ComponentsHTML#License */
+/* Copyright © 2021 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
 namespace YetaWF_Core {
 
--- Core/Skins/Controller/PanelModuleSaveSettings.cs
+++ Core/Skins/Controller/PanelModuleSaveSettings.cs
@@ -1,4 +1,4 @@
-/* Copyright © 2021 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/ComponentsHTML#License */
+/* Copyright © 2021 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Licensing */
 
 using Microsoft.AspNetCore.Mvc;
 using System;
