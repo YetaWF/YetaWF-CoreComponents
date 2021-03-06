c12f9745fab90b4de4d99e26de57b5cbfb9924e2
Mike <mikevdm@mikevdm.com>
2021-03-06 12:41:23 -0500

    experimental debug page load test

--- Core/Addons/_Addons/Basics/Debugging.js
+++ Core/Addons/_Addons/Basics/Debugging.js
@@ -51,6 +51,145 @@ var YetaWF_Core_Debugging;
             inDebug = false;
         }
     };
+    var Links = /** @class */ (function () {
+        function Links() {
+            this.Remaining = [];
+            this.Done = [];
+            this.Running = false;
+            // Anchors withing parent elements with any of these CSS classes are not checked
+            this.IgnoredParentElements = [
+                "Softelvdm_Documentation",
+                "yt_grid", // actions in grids
+            ];
+            // Ignore these Urls
+            this.IgnoreUrls = [
+                "/BlogEntry"
+            ];
+        }
+        // Use actual click handling for anchors in elements with any of these CSS classes
+        // private ActualClicks: string[] = [
+        //     "yt_panels_pagebarinfo_list",
+        // ];
+        Links.prototype.testAll = function () {
+            if (this.isRunning)
+                return;
+            this.Remaining = [];
+            this.Done = [];
+            this.Running = true;
+            this.nextPage();
+        };
+        Links.prototype.nextPage = function () {
+            if (!LinksTest.isRunning)
+                return;
+            var todos = $YetaWF.getElementsBySelector("a");
+            var _loop_2 = function (todo) {
+                var href = todo.href;
+                if (!href || href.startsWith("javascript:"))
+                    return "continue";
+                if ($YetaWF.elementHasClass(todo, "DebugLoadTest"))
+                    return "continue";
+                if ($YetaWF.getAttributeCond(todo, "data-nohref") != null)
+                    return "continue";
+                var target = $YetaWF.getAttributeCond(todo, "target");
+                if (target && target !== "" && target !== "_self")
+                    return "continue";
+                var rel = $YetaWF.getAttributeCond(todo, "rel");
+                if (rel === "nofollow")
+                    return "continue";
+                if (!$YetaWF.elementHasClass(todo, "yaction-link"))
+                    return "continue";
+                var ignore = this_1.IgnoredParentElements.find(function (s) {
+                    return $YetaWF.elementClosestCond(todo, "." + s) != null;
+                });
+                if (ignore)
+                    return "continue";
+                href = this_1.stripUrl(href);
+                var uri = new YetaWF.Url();
+                uri.parse(href);
+                var path = uri.getPath();
+                var foundPath = this_1.IgnoreUrls.find(function (s) {
+                    return path === s;
+                });
+                if (foundPath)
+                    return "continue";
+                var found = this_1.Done.find(function (s) {
+                    return href === s;
+                });
+                if (found)
+                    return "continue";
+                this_1.Remaining.push({ url: window.location.href, anchor: todo });
+            };
+            var this_1 = this;
+            for (var _i = 0, todos_1 = todos; _i < todos_1.length; _i++) {
+                var todo = todos_1[_i];
+                _loop_2(todo);
+            }
+            this.processRemainder();
+        };
+        Links.prototype.stripUrl = function (href) {
+            var uri = new YetaWF.Url();
+            uri.parse(href);
+            uri.removeSearch("!OriginList");
+            uri.setHash(null);
+            href = uri.toUrl();
+            return href;
+        };
+        Links.prototype.processRemainder = function () {
+            var _this = this;
+            var _loop_3 = function () {
+                var linkEntry = this_2.Remaining[0];
+                this_2.Remaining.splice(0, 1);
+                var todo = linkEntry.anchor;
+                var found = this_2.Done.find(function (s) {
+                    return todo.href === s;
+                });
+                if (found)
+                    return "continue";
+                console.log("Debug(" + this_2.Remaining.length + "): Clicking " + todo.href + " on " + linkEntry.url + " - " + todo.outerHTML);
+                this_2.Done.push(this_2.stripUrl(todo.href));
+                // let actual = this.ActualClicks.find((s: string):boolean => {
+                //     return $YetaWF.elementClosestCond(todo, `.${s}`) != null;
+                // });
+                // if (actual) {
+                //     todo.click();// click it and wait for page content
+                // } else {
+                var uri = new YetaWF.Url();
+                uri.parse(todo.href);
+                if ($YetaWF.ContentHandling.setContent(uri, true) !== YetaWF.SetContentResult.ContentReplaced) {
+                    // some how this wasn't possible
+                    console.error("setContent failed for " + todo.href);
+                    setTimeout(function () { _this.nextPage(); }, 1);
+                }
+                return { value: void 0 };
+            };
+            var this_2 = this;
+            while (this.Remaining.length > 0) {
+                var state_1 = _loop_3();
+                if (typeof state_1 === "object")
+                    return state_1.value;
+            }
+            console.log("Debug: All pages processed");
+        };
+        Object.defineProperty(Links.prototype, "isRunning", {
+            get: function () {
+                return this.Running;
+            },
+            enumerable: false,
+            configurable: true
+        });
+        return Links;
+    }());
+    YetaWF_Core_Debugging.Links = Links;
+    // handle new content
+    $YetaWF.registerCustomEventHandlerDocument(YetaWF.Content.EVENTNAVPAGELOADED, null, function (ev) {
+        LinksTest.nextPage();
+        return true;
+    });
+    var LinksTest = new Links();
+    $YetaWF.registerEventHandler($YetaWF.getElement1BySelector(".YetaWF_Menus_MainMenu a.DebugLoadTest"), "click", null, function (ev) {
+        LinksTest.testAll();
+        return false;
+    });
 })(YetaWF_Core_Debugging || (YetaWF_Core_Debugging = {}));
 
 //# sourceMappingURL=Debugging.js.map
--- Core/Addons/_Addons/Basics/Debugging.ts
+++ Core/Addons/_Addons/Basics/Debugging.ts
@@ -56,4 +56,154 @@ namespace YetaWF_Core_Debugging {
         }
     };
 
+    // Experimental "Click All Links" code - not currently used
+    // Starts at current page (when testAll() is called and collects links and "clicks" each one by one)
+    // Used to find JavaScript errors
+
+    interface LinkEntry {
+        url: string;
+        anchor: HTMLAnchorElement;
+    }
+
+    export class Links {
+
+        private Remaining: LinkEntry[] = [];
+        private Done: string[] = [];
+        private Running: boolean = false;
+
+        // Anchors withing parent elements with any of these CSS classes are not checked
+        private IgnoredParentElements: string[] = [
+            "Softelvdm_Documentation", // documentation pages
+            "yt_grid", // actions in grids
+        ];
+        // Ignore these Urls
+        private IgnoreUrls: string[] = [
+            "/BlogEntry"
+        ];
+        // Use actual click handling for anchors in elements with any of these CSS classes
+        // private ActualClicks: string[] = [
+        //     "yt_panels_pagebarinfo_list",
+        // ];
+
+        public testAll(): void {
+            if (this.isRunning) return;
+
+            this.Remaining = [];
+            this.Done = [];
+            this.Running = true;
+
+            this.nextPage();
+        }
+
+        public nextPage(): void {
+
+            if (!LinksTest.isRunning) return;
+
+            let todos = $YetaWF.getElementsBySelector("a") as HTMLAnchorElement[];
+            for (let todo of todos) {
+                let href = todo.href;
+                if (!href || href.startsWith("javascript:"))
+                    continue;
+                if ($YetaWF.elementHasClass(todo, "DebugLoadTest"))
+                    continue;
+                if ($YetaWF.getAttributeCond(todo, "data-nohref") != null)
+                    continue;
+                let target = $YetaWF.getAttributeCond(todo, "target");
+                if (target && target !== "" && target !== "_self")
+                    continue;
+                let rel = $YetaWF.getAttributeCond(todo, "rel");
+                if (rel === "nofollow")
+                    continue;
+                if (!$YetaWF.elementHasClass(todo, "yaction-link"))
+                    continue;
+
+                let ignore = this.IgnoredParentElements.find((s: string):boolean => {
+                    return $YetaWF.elementClosestCond(todo, `.${s}`) != null;
+                });
+                if (ignore)
+                    continue;
+
+                href = this.stripUrl(href);
+
+                let uri = new YetaWF.Url();
+                uri.parse(href);
+                let path = uri.getPath();
+                let foundPath = this.IgnoreUrls.find((s: string):boolean => {
+                    return path === s;
+                });
+                if (foundPath)
+                    continue;
+
+                let found = this.Done.find((s: string):boolean => {
+                    return href === s;
+                });
+                if (found)
+                    continue;
+
+                this.Remaining.push({ url: window.location.href, anchor: todo});
+            }
+            this.processRemainder();
+        }
+
+        private stripUrl(href: string): string {
+            let uri = new YetaWF.Url();
+            uri.parse(href);
+            uri.removeSearch("!OriginList");
+            uri.setHash(null);
+            href = uri.toUrl();
+            return href;
+        }
+
+        private processRemainder(): void {
+            while (this.Remaining.length > 0) {
+
+                let linkEntry = this.Remaining[0];
+                this.Remaining.splice(0, 1);
+
+                let todo = linkEntry.anchor;
+
+                let found = this.Done.find((s: string):boolean =>{
+                    return todo.href === s;
+                });
+                if (found)
+                    continue;
+
+                console.log(`Debug(${this.Remaining.length}): Clicking ${todo.href} on ${linkEntry.url} - ${todo.outerHTML}`);
+                this.Done.push(this.stripUrl(todo.href));
+
+                // let actual = this.ActualClicks.find((s: string):boolean => {
+                //     return $YetaWF.elementClosestCond(todo, `.${s}`) != null;
+                // });
+                // if (actual) {
+                //     todo.click();// click it and wait for page content
+                // } else {
+                let uri = new YetaWF.Url();
+                uri.parse(todo.href);
+                if ($YetaWF.ContentHandling.setContent(uri, true) !== YetaWF.SetContentResult.ContentReplaced) {
+                    // some how this wasn't possible
+                    console.error(`setContent failed for ${todo.href}`);
+                    setTimeout(():void => { this.nextPage(); }, 1);
+                }
+                //}
+
+                return;
+            }
+            console.log("Debug: All pages processed");
+        }
+        public get isRunning(): boolean {
+            return this.Running;
+        }
+    }
+
+    // handle new content
+    $YetaWF.registerCustomEventHandlerDocument(YetaWF.Content.EVENTNAVPAGELOADED, null, (ev: CustomEvent<YetaWF.DetailsEventNavPageLoaded>): boolean => {
+        LinksTest.nextPage();
+        return true;
+    });
+
+    let LinksTest = new Links();
+    $YetaWF.registerEventHandler($YetaWF.getElement1BySelector(".YetaWF_Menus_MainMenu a.DebugLoadTest"), "click", null, (ev: MouseEvent): boolean => {
+        LinksTest.testAll();
+        return false;
+    });
 }
