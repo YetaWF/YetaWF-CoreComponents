c38fb2bfb0ca89f7ee2b6e5c141f0d990ae12da1
Mike <mikevdm@mikevdm.com>
2022-03-28 08:29:31 -0400

    -> HttpClient

--- Core/Models/Attributes/RecaptchaV2Attribute.cs
+++ Core/Models/Attributes/RecaptchaV2Attribute.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Net;
+using System.Net.Http;
 using YetaWF.Core.Components;
 using YetaWF.Core.Localize;
 using YetaWF.Core.Log;
@@ -23,13 +23,21 @@ namespace YetaWF.Core.Models.Attributes {
 
         private static string __ResStr(string name, string defaultValue, params object?[] parms) { return ResourceAccess.GetResourceString(typeof(Resources), name, defaultValue, parms); }
 
+        private static readonly HttpClientHandler Handler = new HttpClientHandler {
+            AllowAutoRedirect = true,
+            UseCookies = false,
+        };
+        private static readonly HttpClient Client = new HttpClient(Handler, true) {
+            Timeout = new TimeSpan(0, 0, 20),
+        };
+
         public RecaptchaV2Attribute(string message) : base(message) {
             ErrorMessage = message;
         }
         private new string ErrorMessage { get; set; }
 
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext) {
-            if (!Manager.HaveCurrentRequest) throw new InternalError("No http context or request available");
+            if (!Manager.HaveCurrentRequest) throw new InternalError("No HTTP context or request available");
             HttpRequest request = Manager.CurrentRequest;
 
             RecaptchaV2Data? rc = value as RecaptchaV2Data;
@@ -56,14 +64,21 @@ namespace YetaWF.Core.Models.Attributes {
             public List<string>? ErrorCodes { get; set; }
         }
         private bool ValidateCaptcha(RecaptchaV2Config config, string response, string ipAddress) {
-            using (WebClient client = new WebClient()) {
-                if (string.IsNullOrWhiteSpace(config.PublicKey) || string.IsNullOrWhiteSpace(config.PrivateKey))
-                    throw new InternalError("The public and/or private keys have not been configured for RecaptchaV2 handling");
-                string resp = client.DownloadString(string.Format("https://www.google.com/recaptcha/api/siteverify?secret={0}&response={1}&remoteIp={2}",
-                    config.PrivateKey, response, ipAddress));
-                Logging.AddLog("Validating CaptchaV2 - received \"{0}\"", resp);
-                RecaptchaV2Response recaptchaResp = Utility.JsonDeserialize<RecaptchaV2Response>(resp);
-                return recaptchaResp.Success;
+
+            if (string.IsNullOrWhiteSpace(config.PublicKey) || string.IsNullOrWhiteSpace(config.PrivateKey))
+                throw new InternalError("The public and/or private keys have not been configured for RecaptchaV2 handling");
+
+            string? resp = null;
+            try {
+                using (var request = new HttpRequestMessage()) {
+                    resp = Client.GetStringAsync($"https://www.google.com/recaptcha/api/siteverify?secret={config.PrivateKey}&response={response}&remoteIp={ipAddress}").Result;
+                    Logging.AddLog($"Validating CaptchaV2 - received \"{resp}\"");
+                    RecaptchaV2Response recaptchaResp = Utility.JsonDeserialize<RecaptchaV2Response>(resp);
+                    return recaptchaResp.Success;
+                }
+            } catch (Exception exc) {
+                Console.WriteLine(exc.Message);
+                throw;
             }
         }
     }
